2ef40a69a5399e556c5b3cef7451627054fbe2ae
shifawe <451154708@qq.com>
2019-11-28 17:10:34 +0800

    添加 TagsView,   完美列表编辑

--- src/api/table.js
+++ src/api/table.js
@@ -1,9 +1,17 @@
 import request from '@/utils/request'
 
-export function fetchData(params) {
+export function fetchData(query) {
   return request({
     url: '/table/list',
     method: 'get',
-    params
+    params: query
+  })
+}
+
+export function fetchArticle(id) {
+  return request({
+    url: '/table/detail',
+    method: 'get',
+    params: { id }
   })
 }
--- src/views/table/edit.vue
+++ src/views/table/edit.vue
@@ -5,14 +5,14 @@
         <el-row :gutter="25">
           <el-col :span="8">
             <el-form-item label="作者:">
-              <el-input v-model="formInline.user" placeholder="作者"></el-input>
+              <el-input v-model="formInline.author" placeholder="作者"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="日期:">
               <div class="block-group">
                 <el-date-picker
-                  v-model="formInline.date"
+                  v-model="formInline.display_time"
                   type="datetime"
                   placeholder="选择日期时间"
                   >
@@ -24,12 +24,11 @@
             <el-form-item label="评分:">
               <div class="block-group">
                 <el-rate 
-                  v-model="formInline.rateVal"
-                  :colors="formInline.colors"
+                  v-model="formInline.importance"
+                  :colors="colors"
                 >
                 </el-rate> 
               </div>
-              
             </el-form-item>
           </el-col>
         </el-row>
@@ -38,7 +37,7 @@
             type="textarea"
             :rows="2"
             placeholder="请输入内容"
-            v-model="formInline.textarea">
+            v-model="formInline.content_short">
           </el-input>
         </el-form-item>
         <el-form-item class="btn-group">
@@ -51,17 +50,35 @@
 </template>
 
 <script >
+
+import { fetchArticle } from '@/api/table'
+
+const defaultForm = {
+  id: undefined,
+  title: '', // 标题
+  author: '', // 作者
+  display_time: '', // 日期
+  content_short: '', // 内容摘要
+  importance: null, // 评分
+}
+
 export default {
   name: 'Edit',
   data() {
     return {
-      formInline: {
-        user: '',
-        date: '',
-        textarea: '',
-        rateVal: null,
-        colors: ['#99A9BF', '#F7BA2A', '#f00'] 
-      }
+      formInline: Object.assign({}, defaultForm),
+      colors: ['#99A9BF', '#F7BA2A', '#f00']
+    }
+  },
+  created() {
+    const id = this.$route.params && this.$route.params.id
+    this.fatchList(id)
+  },
+  methods: {
+    fatchList(id) {
+      fetchArticle(id).then( response => {
+        this.formInline = response.data
+      })
     }
   }
 }
@@ -78,4 +95,5 @@ export default {
   }
   .el-date-editor.el-input{width:100%}
 }
+
 </style>
